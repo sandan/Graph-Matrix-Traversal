31cb932f8bf9e52931b470575ce20cf441b5dc23
Mark Sandan <msandan@utexas.edu>
2015-03-22 20:19:21 -0500

    refactor the dijkstra algorithm out of src files

--- src/dense/PQ_ssp_half.c
+++ src/dense/PQ_ssp_half.c
@@ -3,35 +3,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <malloc.h>
-#include <assert.h>
 #include <papi.h>
 #include "../../include/priority_q.h"
+#include "../../include/header.h"
 
 struct pq priorty_queue;
 
 /* allocate space for RING,HALF, and vertices array for the queue */
 vertex vertices[N]; //N is set at compile time
 int HALF[N][N];       //adj matrix rep of half-connected graph
-
-void adj_lvl_labeling(struct vertex source, int G[N][N]){
- source.label=0;
- insert(&vertexQueue,source.label);
- while(vertexQueue.size!=0){
-   struct vertex* vertex=Dequeue(&nodeQueue);
-   int label=extractmin(&vertexQueue);
-   int row=vertex->id,neighbor;
-   for (neighbor=0;neighbor<N;neighbor++){
-     if (G[row][neighbor]!=0){
-       vertices[neighbor].visited=1;
-       if (vertices[neighbor].label>vertex->label+label){
-         vertices[neighbor].label=vertex->label+label;
-         Enqueue(&vertexQueue,&vertices[neighbor]);
-         insert(&vertexQueue,vertices[neighbor].label);
-       }
-     }
-   }
- }
-}
 
 /* zero out FLUSH_SIZE bytes in memory*/
 int flush(){
@@ -49,8 +29,10 @@ void setup(){
  int i;
  /*initialize the vertices array to access vertexs*/
  for (i=0; i<N; ++i){
-   struct vertex v;
+   vertex v;
    v.id=i;
+   v.unvisited = true;
+   v.label = INT_MAX;
    vertices[i]=v;
  }
   /*create the HALF graph*/
@@ -59,7 +41,7 @@ void setup(){
   row=vertices[i].id;
   for(j=0; j<N;j++){
    if(rand()%2==1){
-    HALF[row][j]=1;
+    HALF[row][j]=1; //should really add weights than just 1...
    }
   }
  }
@@ -69,53 +51,46 @@ void handle_error(int retval){
   printf("PAPI error %d: %s\n",retval,PAPI_strerror(retval));
   exit(1);
 }
-void doHALF(){
-
-/*event measued: PAPI_L1_DCM*/
-int retval, EventSet=PAPI_NULL;
-long_long values[1];
-
-/* initialize the PAPI library */
-retval = PAPI_library_init(PAPI_VER_CURRENT);
-if (retval != PAPI_VER_CURRENT) {
-  fprintf(stderr, "PAPI library init error!\n");
-  exit(1);
-}
-
-/* create Event Set */
-if (PAPI_create_eventset(&EventSet) != PAPI_OK)
-        handle_error(1);
-
-/* add Total Instructions Executed to our Event Set */
-if (PAPI_add_event(EventSet, PAPI_L1_DCM) != PAPI_OK)
-        handle_error(1);
-
-/* initialize the Graphs*/
-setup();
-
-/*flush the cache 32Kb*/
-flush();
-
-/* start counting events in the Event Set */
-if (PAPI_start(EventSet) != PAPI_OK)
-        handle_error(1);
-
-/* BFS level labeling */
-adj_lvl_labeling(vertices[0],HALF);
-
-/* stop counting in the Event Set */
-if (PAPI_stop(EventSet, values) != PAPI_OK)
-        handle_error(1);
-
-/*report the result*/
-printf("%lld\n",values[0]);
-
-}
 
 int main(){
- doHALF();
-return 0;
+  /*event measued: PAPI_L1_DCM*/
+  int retval, EventSet=PAPI_NULL;
+  long_long values[1];
+  
+  /* initialize the PAPI library */
+  retval = PAPI_library_init(PAPI_VER_CURRENT);
+  if (retval != PAPI_VER_CURRENT) {
+    fprintf(stderr, "PAPI library init error!\n");
+    exit(1);
+  }
+  
+  /* create Event Set */
+  if (PAPI_create_eventset(&EventSet) != PAPI_OK)
+          handle_error(1);
+  
+  /* add Total Instructions Executed to our Event Set */
+  if (PAPI_add_event(EventSet, PAPI_L1_DCM) != PAPI_OK)
+          handle_error(1);
+  
+  /* initialize the Graphs*/
+  setup();
+  
+  /*flush the cache 32Kb*/
+  flush();
+  
+  /* start counting events in the Event Set */
+  if (PAPI_start(EventSet) != PAPI_OK)
+          handle_error(1);
+  
+  /* BFS level labeling */
+  adj_lvl_labeling(&pq, vertices, HALF);
+  
+  /* stop counting in the Event Set */
+  if (PAPI_stop(EventSet, values) != PAPI_OK)
+          handle_error(1);
+  
+  /*report the result*/
+  printf("%lld\n",values[0]);
+  
+  }
 }
-
-
-
--- src/dense/PQ_ssp_ring.c
+++ src/dense/PQ_ssp_ring.c
@@ -6,32 +6,9 @@
 #include <assert.h>
 #include <papi.h>
 
-struct PQueue nodeQueue;
-
-/* allocate space for RING,HALF, and nodes array for the queue */
-struct node nodes[N]; //N is set at compile time
+/* allocate space for RING,HALF, and vertexs array for the queue */
+vertex vertices[N]; //N is set at compile time
 int RING[N][N];       //adjacency matrix representation of ring graph
-
-void adj_lvl_labeling(struct node source, int G[N][N]){
- source.label=0;
- Enqueue(&nodeQueue,&source);
- insert(&nodeQueue,source.label);
- while(nodeQueue.size!=0){
-   struct node* vertex=Dequeue(&nodeQueue);
-   int label=extractmin(&nodeQueue);
-   int row=vertex->id,neighbor;
-   for (neighbor=0;neighbor<N;neighbor++){
-     if (G[row][neighbor]!=0){
-       nodes[neighbor].visited=1;
-       if (nodes[neighbor].label>vertex->label+label){
-         nodes[neighbor].label=vertex->label+label;
-         Enqueue(&nodeQueue,&nodes[neighbor]);
-         insert(&nodeQueue,nodes[neighbor].label);
-       }
-     }
-   }
- }
-}
 
 /* zero out FLUSH_SIZE bytes in memory*/
 int flush(){
@@ -47,73 +24,70 @@ return 0;
 void setup(){
  srand(time(NULL));
  int i;
- /*initialize the nodes array to access nodes*/
+ /*initialize the vertexs array to access nodes*/
  for (i=0; i<N; ++i){
-   struct node v;
+   vertex v;
+   v.unvisited = true;
+   v.label = INT_MAX;
    v.id=i;
-   nodes[i]=v;
+   vertices[i]=v;
  }
+
  /*create the RING graph*/
  int row;
  for (i=0; i<N-1; i++){
-  row=nodes[i].id;
+  row=vertices[i].id;
   RING[row][row+1]=1;
  }
- row=nodes[i+1].id;
+ row=vertices[i+1].id;
  RING[row][0]=1; 
 }
+
 void handle_error(int retval){
   printf("PAPI error %d: %s\n",retval,PAPI_strerror(retval));
   exit(1);
 }
 
-void doRING(){
-
-/*event measued: PAPI_L1_DCM*/
-int retval, EventSet=PAPI_NULL;
-long_long values[1];
-
-/* initialize the PAPI library */
-retval = PAPI_library_init(PAPI_VER_CURRENT);
-if (retval != PAPI_VER_CURRENT) {
-  fprintf(stderr, "PAPI library init error!\n");
-  exit(1);
-}
-
-/* create Event Set */
-if (PAPI_create_eventset(&EventSet) != PAPI_OK)
-        handle_error(1);
-
-/* add Total Instructions Executed to our Event Set */
-if (PAPI_add_event(EventSet, PAPI_L1_DCM) != PAPI_OK)
-        handle_error(1);
-
-/* initialize the Graphs*/
-setup();
-
-/*flush the cache 32Kb*/
-flush();
-
-/* start counting events in the Event Set */
-if (PAPI_start(EventSet) != PAPI_OK)
-        handle_error(1);
-
-/* BFS level labeling */
-adj_lvl_labeling(nodes[0],RING);
-
-/* stop counting in the Event Set */
-if (PAPI_stop(EventSet, values) != PAPI_OK)
-        handle_error(1);
-
-/*report the result*/
-printf("%lld\n",values[0]);
-
-}
-
 int main(){
- doRING();
-return 0;
+  
+  /*event measued: PAPI_L1_DCM*/
+  int retval, EventSet=PAPI_NULL;
+  long_long values[1];
+  
+  /* initialize the PAPI library */
+  retval = PAPI_library_init(PAPI_VER_CURRENT);
+  if (retval != PAPI_VER_CURRENT) {
+    fprintf(stderr, "PAPI library init error!\n");
+    exit(1);
+  }
+  
+  /* create Event Set */
+  if (PAPI_create_eventset(&EventSet) != PAPI_OK)
+          handle_error(1);
+  
+  /* add Total Instructions Executed to our Event Set */
+  if (PAPI_add_event(EventSet, PAPI_L1_DCM) != PAPI_OK)
+          handle_error(1);
+  
+  /* initialize the Graphs*/
+  setup();
+  
+  /*flush the cache 32Kb*/
+  flush();
+  
+  /* start counting events in the Event Set */
+  if (PAPI_start(EventSet) != PAPI_OK)
+          handle_error(1);
+  
+  /* BFS level labeling */
+  adj_lvl_labeling(&pq, vertices,RING);
+  
+  /* stop counting in the Event Set */
+  if (PAPI_stop(EventSet, values) != PAPI_OK)
+          handle_error(1);
+  
+  /*report the result*/
+  printf("%lld\n",values[0]);
+  
+  return 0;
 }
-
-
-
